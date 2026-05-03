d2f8dcd3ff0384f9ed17158489fb5f4956f201de
LoganHere <twopaer31@gmail.com>
2026-05-03 19:07:26 +0300

    Создать стартер логов

---
+++ auto-log-starter/src/main/java/com/loganhere/auto_log_starter/LogLevel.java
@@ -0,0 +1,9 @@
+package com.loganhere.auto_log_starter;
+
+public enum LogLevel {
+    TRACE,
+    DEBUG,
+    INFO,
+    WARN,
+    ERROR
+}
--- src/main/java/com/loganhere/usercard/client/EmailValidationClient.java
+++ src/main/java/com/loganhere/usercard/client/EmailValidationClient.java
@@ -9,6 +9,6 @@ import org.springframework.web.bind.annotation.RequestBody;
 @FeignClient(name = "email-validation-service", url = "${validate.server.url}")
 public interface EmailValidationClient {
 
-    @PostMapping
+    @PostMapping("/email/validate")
     EmailValidationResponse validateEmail(@RequestBody EmailValidateRequest request);
 }
